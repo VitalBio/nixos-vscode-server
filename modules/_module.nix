93d938d863d3739ad25999860d69446599a42ded
Ross MacLeod <ross@vitalbio.com>
2023-09-08 21:24:09 +0000

    vscode-cli: import mkDefault

--- modules/_module.nix
+++ modules/_module.nix
@@ -1,4 +1,5 @@
 moduleConfig: { lib, ... }: let
+  inherit (lib) mkDefault;
   mkVSCodeService = import ../lib/mkVSCodeService.nix;
 in {
   vscode-cli = mkVSCodeService "cli" { installPath = mkDefault "~/.vscode-cli/code-stable"; };
